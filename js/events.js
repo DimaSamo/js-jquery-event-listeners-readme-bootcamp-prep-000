0193ae5fcf99902375a1ee98938dc23a81254b50
Dmitri Samotessov <dimasamo96@gmail.com>
2018-10-16 17:39:21 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -11,19 +11,7 @@ $('img').on("load", function(){
 })
 }
 
-function pressIt(){
-  $('form').on("keydown", function(key){
-    if (key.which===71){
-      alert("You pressed \"G\".")
-    }
-  })
-}
 
-function submitIt(){
-  $('form').on("submit", function(){
-    alert("Your form is going to be submitted now.")
-  })
-}
 
 
 $(document).ready(function(){
